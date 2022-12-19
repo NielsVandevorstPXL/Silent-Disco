5c7891ec393b828bcf2d3b4b90f4a62d2af4a8b4
NielsVandevorstPXL <116808666+NielsVandevorstPXL@users.noreply.github.com>
2022-12-19 13:18:28 +0100

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -1,5 +1,5 @@
 <?php
-
+if (isset($_POST['submit'])){
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
@@ -22,4 +22,5 @@ $headers .="Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
+}
 ?>
